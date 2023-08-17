97662362af7beca3f1e4c4bdce9825ce7371ea8d
ELLEN VITORIA DE JESUS SILVA <ellen.vjsilva@senacsp.edu.br>
2023-08-17 12:28:52 -0300

    atualização form

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", success_message=None)
 
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == "POST":
+        nome = request.form.get('nome')
+        email = request.form.get('email')
+        telefone = request.form.get('telefone')
+        msg = request.form.get('msg')
+        
+        # Faça algo com os dados, se necessário
+        
+        success_message = "Envio do formulário com sucesso!"
+        return render_template('index.html', success_message=success_message)
 
 if __name__ == "__main__":
     app.run(debug=True)
